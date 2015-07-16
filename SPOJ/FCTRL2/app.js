c7b96b35363ce35b97b29ed9489a506429674979
John-Philip Johansson <seriema@gmail.com>
2015-07-16 08:58:54 +0200

    Encapsulate BigInt for reuse.

--- SPOJ/FCTRL2/app.js
+++ SPOJ/FCTRL2/app.js
@@ -2,83 +2,85 @@
 
 var endOfLine = require('os').EOL;
 
+var BigInt = {
+	create: function (regularInt) {
+		var strArr = regularInt.toString().split('');
+		var intArr = strArr.map(function (str) {
+			return parseInt(str, 10);
+		});
 
-var createBigInt = function (regularInt) {
-	var strArr = regularInt.toString().split('');
-	var intArr = strArr.map(function (str) {
-		return parseInt(str, 10);
-	});
+		return intArr;
+	},
 
-	return intArr;
-};
+	toString: function (bigInt) {
+		return bigInt.join('');
+	},
 
-var bigIntToString = function (bigInt) {
-	return bigInt.join('');
-}
+	add: function (a1, a2) {
+		var sum = [];
 
-var addBigInt = function (a1, a2) {
-	var sum = [];
-
-	var largerInt = a1.length >= a2.length ? a1 : a2;
-	var smallerInt = a1.length < a2.length ? a1 : a2;
-
-	var carry = 0;
-	for (var i = smallerInt.length-1, j = largerInt.length-1; j >= 0; i--, j--) {
-		var tmp = largerInt[j] + carry;
-
-		if (i >= 0) {
-			tmp += smallerInt[i];
-		}
-
-		var rest = tmp % 10;
-		sum.unshift(rest);
-		carry = Math.floor(tmp / 10);
-	}
-
-	if (carry > 0)
-		sum.unshift(carry);
-
-	return sum;
-};
-
-var multiplyBigInt = function (f1, f2) {
-	var largerInt = f1.length >= f2.length ? f1 : f2;
-	var smallerInt = f1.length < f2.length ? f1 : f2;
-
-	var product = createBigInt(0);
-	for (var j = smallerInt.length-1; j >= 0; j--) {
-		var partialProduct = [];
-		for (var grow=0; grow < smallerInt.length-j-1; grow++) {
-			partialProduct.push(0);
-		}
+		var largerInt = a1.length >= a2.length ? a1 : a2;
+		var smallerInt = a1.length < a2.length ? a1 : a2;
 
 		var carry = 0;
-		for (var i = largerInt.length-1; i >= 0; i--) {
-			var tmp = largerInt[i] * smallerInt[j] + carry;
-			var rest = tmp % 10;
+		for (var i = smallerInt.length-1, j = largerInt.length-1; j >= 0; i--, j--) {
+			var tmp = largerInt[j] + carry;
 
+			if (i >= 0) {
+				tmp += smallerInt[i];
+			}
+
+			var rest = tmp % 10;
+			sum.unshift(rest);
 			carry = Math.floor(tmp / 10);
-			partialProduct.unshift(rest);
 		}
 
 		if (carry > 0)
-			partialProduct.unshift(carry);
+			sum.unshift(carry);
 
-		product = addBigInt(product, partialProduct);
+		return sum;
+	},
+
+	multiply: function (f1, f2) {
+		var largerInt = f1.length >= f2.length ? f1 : f2;
+		var smallerInt = f1.length < f2.length ? f1 : f2;
+
+		var product = BigInt.create(0);
+		for (var j = smallerInt.length-1; j >= 0; j--) {
+			var partialProduct = [];
+			var grow = smallerInt.length-j;
+			while(--grow) {
+				partialProduct.push(0);
+			}
+
+			var carry = 0;
+			for (var i = largerInt.length-1; i >= 0; i--) {
+				var tmp = largerInt[i] * smallerInt[j] + carry;
+				var rest = tmp % 10;
+
+				carry = Math.floor(tmp / 10);
+				partialProduct.unshift(rest);
+			}
+
+			if (carry > 0)
+				partialProduct.unshift(carry);
+
+			product = BigInt.add(product, partialProduct);
+		}
+
+		return product;
 	}
-
-	return product;
 };
 
 var calcFactorial = function (factorialTarget, cache) {
 	if (factorialTarget === 0)
-		return createBigInt(1);
+		return BigInt.create(1);
 
 	if (cache[factorialTarget])
 		return cache[factorialTarget];
 
 	var nextFactorial = calcFactorial(factorialTarget-1, cache);
-	var factorial = multiplyBigInt(nextFactorial, createBigInt(factorialTarget));
+	var factorial = BigInt.multiply(nextFactorial, BigInt.create(factorialTarget));
 	cache[factorialTarget] = factorial;
 	return factorial;
 };
@@ -93,7 +95,7 @@ var main = function (lines) {
 		var factorialTarget = parseInt(lines.next(), 10);
 		var factorial = calcFactorial(factorialTarget, cache);
 
-		writeLn(bigIntToString(factorial));
+		writeLn(BigInt.toString(factorial));
 	}
 };
 
@@ -146,6 +148,6 @@ process.stdin.on('end', function(){
 
 
 // --- EXPORT METHODS FOR DEBUG HERE
-exports.createBigInt = createBigInt;
-exports.multiplyBigInt = multiplyBigInt;
-exports.addBigInt = addBigInt;
+exports.createBigInt = BigInt.create;
+exports.multiplyBigInt = BigInt.multiply;
+exports.addBigInt = BigInt.add;
